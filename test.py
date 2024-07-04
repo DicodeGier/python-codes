4db0293aab1093e17829999a8cfb5081f5b055e9
Dico <dico.de.gier@gmail.com>
2024-07-04 12:29:41 +0200

    test in test.py

--- test.py
+++ test.py
@@ -4,13 +4,4 @@ dictionary = {'Rotterdam': {'4': {'best': {'model': 'ARIMA_func'}}}}
 lst.append(re.split("_func",dictionary["Rotterdam"]["4"]["best"]["model"])[0])
 print(lst)
 
-#create an empty list 
-lst = []
-
-#append to the list
-for i in range(5):
-    for j in range(5):
-        lst.append((i,j))
-
-print(lst)
 
