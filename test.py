9baa9e574ac0edf05415a728fa18d8f6c7cc8815
Dico <dico.de.gier@gmail.com>
2024-07-04 12:36:19 +0200

    another test

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 import regex as re 
+import numpy as np
 lst = []
 dictionary = {'Rotterdam': {'4': {'best': {'model': 'ARIMA_func'}}}}
 lst.append(re.split("_func",dictionary["Rotterdam"]["4"]["best"]["model"])[0])
