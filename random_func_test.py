78ace85629e961ed29be4a74fa323d0775798af3
Dico <dico.de.gier@gmail.com>
2024-07-21 12:43:51 +0200

    renamed file for testing

--- random_func_test.py
+++ random_func_test.py
@@ -3,4 +3,4 @@ import random_func
 
 class TestRandomFunc(unittest.TestCase):
     def test_random_func(self):
-        self.assertEqual(random_func(5), 10, "random_func(5) should return 10")
+        self.assertEqual(random_func.random_func(5), 10, "random_func(5) should return 10")
