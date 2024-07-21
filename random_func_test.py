74949bd00399799f50143e92619fab0acb4737a4
Dico <dico.de.gier@gmail.com>
2024-07-21 13:20:29 +0200

    subtesting random func

--- random_func_test.py
+++ random_func_test.py
@@ -3,4 +3,9 @@ import random_func
 
 class TestRandomFunc(unittest.TestCase):
     def test_random_func(self):
-        self.assertEqual(random_func.random_func(5), 10, "random_func(5) should return 10")
+        for num in [5,10,15]:
+            with self.subTest(num=num):
+                self.assertEqual(random_func.random_func(num), num+5, "random_func({}) should return {}".format(num,num+5))
+
+if __name__ == '__main__':
+    unittest.main()
