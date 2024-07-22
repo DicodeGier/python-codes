e01948db528db08d1fd9a185d6e24e621b7a65e7
Dico <dico.de.gier@gmail.com>
2024-07-22 17:10:44 +0200

    test to see if other laptop is syncing

--- test.py
+++ test.py
@@ -1,8 +1,3 @@
-import regex as re 
-import numpy as np
-lst = []
-dictionary = {'Rotterdam': {'4': {'best': {'model': 'ARIMA_func'}}}}
-lst.append(re.split("_func",dictionary["Rotterdam"]["4"]["best"]["model"])[0])
-print(lst)
+#test 
 
 
